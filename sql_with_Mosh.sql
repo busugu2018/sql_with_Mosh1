a1db47a59eae812609ca865f3eacc9989d6a39f5
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2023-08-23 22:26:19 -0500

    commit 1: Introduction 0:01:02 What is SQL? 0:04:24 Cheat Sheet 0:04:50 Installing MySQL on Mac 0:09:48 Installing MySQL on Windows 0:15:08 Creating the Databases for this Course 0:23:40 The SELECT Statement 0:29:30 The SELECT Clause 0:38:18 The WHERE Clause 0:43:35 The AND, OR, and NOT Operators

---
+++ sql_with_Mosh.sql
@@ -0,0 +1,129 @@
+--SQL review
+
+SELECT * from sql_store.customers;
+
+USE sql_store;
+
+SELECT first_name, 
+last_name, 
+points, 
+points+10, 
+((points*10)+100) 
+from sql_store.customers;
+
+SELECT  
+	first_name,  
+    last_name,  
+    points,  
+    points+10,  
+    ((points*10)+100)  
+from customers 
+where customer_id = 1;
+
+
+
+SELECT  
+	first_name,  
+    last_name,  
+    points,  
+    points+10,  
+    ((points*10)+100)  
+    from customers 
+    -- where customer_id = 10;
+;
+
+select * from orders;
+
+SELECT   
+	first_name,   
+    last_name,   
+    points,   
+    points+10,   
+    ((points*10)+100) 
+as discount_factor 
+from customers ;
+
+
+select 
+	state 
+from customers;
+
+
+
+select * 
+from orders;
+
+
+
+select * 
+from products;
+
+
+
+select 
+	name, 
+    unit_price, 
+    unit_price * 1.1  
+from products;
+
+
+select *  
+from orders 
+where oreder_date >= '2019-01-01';
+
+
+select *  
+from orders
+where order_date >= '2018-01-01';
+
+
+select * 
+from customers 
+where birth_date > '1990-01-01';
+
+
+
+select * 
+from customers 
+where birth_date > '1990-01-01' or points > 1000;
+
+select * 
+from customers 
+where birth_date > '1990-01-01' or points > 1000 and state = 'VA';
+
+
+select * 
+from customers 
+where not (birth_date > '1990-01-01' or points > 1000);
+
+
+select * 
+from customers 
+where birth_date <= '1990-01-01' and points > 1000;
+
+
+SELECT * 
+FROM sql_store.orders;
+
+SELECT * 
+FROM sql_store.order_items;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
