5326ee2c8f3ac5368dfe1e2525d9f62f02d6020f
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-06-11 12:42:57 -0500

    commit 3: 1:03:21 time passed.

--- sql_with_Mosh.sql
+++ sql_with_Mosh.sql
@@ -115,6 +115,10 @@ select *
 from customers 
 where birth_date > '1990-01-01' or points > 1000 and state = 'VA';
 
+select * 
+from customers 
+where birth_date > '1990-01-01' or 
+	  (points > 1000 and state = 'VA');
 
 select * 
 from customers 
@@ -132,17 +136,84 @@ FROM sql_store.orders;
 SELECT * 
 FROM sql_store.order_items;
 
+select * 
+from order_items 
+where order_id = 6 and 
+	(quantity * unit_price > 30);
+
+select * 
+from customers
+where state = 'VA' OR state = 'GA' OR state = 'FL';
+
+-- use this instead
+-- IN operator
+
+select * 
+from customers
+where state in ('VA', 'GA','FL');
+
+select * 
+from customers
+where state not in ('VA', 'GA','FL');
+
+select * 
+from products
+where quantity_in_stock in (49, 38, 72);
+
+
+-- between operator
+
+select * 
+from customers
+where points between 1000 and 3000;
+
+select * 
+from customers
+where birth_date between '1990-1-1' and '2000-1-1';
 
 
 
 
+-- like operator
+-- % operator to signal any number of character
+-- _ operator to signal a single character
+-- REGEXP 
 
+select * 
+from customers
+where last_name like 'b%';
 
+select * 
+from customers
+where last_name like 'brush%';
 
+select * 
+from customers
+where last_name like '%b%';
 
+select * 
+from customers
+where last_name like '%y';
 
+select * 
+from customers
+where last_name like '_____y';
 
+select * 
+from customers
+where last_name like 'b____y';
 
+select *
+from customers
+where address like '%TRAIL%' or address like 'AVENUE';
+
+select *
+from customers
+where phone like '%9'; 
+
+select *
+from customers
+where phone not like '%9'; 
 
 
 
