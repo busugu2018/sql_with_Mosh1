d7859686f1c727d716c263de1e73dd10fa41e136
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-06-14 12:26:43 -0500

    commit 3: 1:19:47

--- sql_with_Mosh.sql
+++ sql_with_Mosh.sql
@@ -162,6 +162,7 @@ where quantity_in_stock in (49, 38, 72);
 
 
 -- between operator
+-- birth_date in MySql is written 'year-month-day'
 
 select * 
 from customers
@@ -177,8 +178,7 @@ where birth_date between '1990-1-1' and '2000-1-1';
 -- like operator
 -- % operator to signal any number of character
 -- _ operator to signal a single character
--- REGEXP 
-
+-- REGEXP: regular expression
 select * 
 from customers
 where last_name like 'b%';
@@ -211,14 +211,178 @@ select *
 from customers
 where phone like '%9'; 
 
+-- same 👆
+
 select *
 from customers
 where phone not like '%9'; 
 
 
+-- REGEXP: regular expression
+ 
+-- 1
+select *
+from customers
+where last_name like '%field%'; 
+
+
+-- 2
+select *
+from customers
+where last_name REGEXP 'field'; 
+
+-- 1 AND -- 2 are the same
+
+-- STILL REGEXP
+
+-- "^" signifies Begining of a string
+
+-- "$" signifiessignifies End of a string
+
+-- "|" signifies logical "or"
+
+-- [abcs]
+
+-- [a-s]
+
+select *
+from customers
+where last_name REGEXP '^field'; 
+
+select *
+from customers
+where last_name REGEXP 'field$'; 
+
+select *
+from customers
+where last_name REGEXP 'field|mac'; 
+
+select *
+from customers
+where last_name REGEXP 'field|mac|rose'; 
+
+select *
+from customers
+where last_name REGEXP '^field|mac|rose'; 
+
+select *
+from customers
+where last_name REGEXP 'field$|mac|rose'; 
+
+select *
+from customers
+where last_name REGEXP '[gim]e'; -- ge, ie, me, every character that come before e
+
+select *
+from customers
+where last_name REGEXP '[gim]e';-- ge, ie, me, every character that come after e
+
+select *
+from customers
+where last_name REGEXP '[a-h]e';
+
+select *
+from customers
+where last_name REGEXP 'e[a-h]';
 
 
 
+-- Get the customers whose 
+-- 		first names are Elka or Ambur
+-- 		last names end with EY or ON
+-- 		last names start with MY or contains SE 
+-- 		last names contain B followed by R or U
+
+
+
+-- Solutions
+
+select *
+from customers 
+where first_name REGEXP 'Elka|Ambur';
+
+select *
+from customers 
+where last_name REGEXP 'EY$|ON$';
+
+select *
+from customers 
+where last_name REGEXP '^MY|SE';
+
+select *
+from customers 
+where last_name REGEXP 'B[R|U]';
+
+-- same 👆
+select *
+from customers 
+where last_name REGEXP 'B[RU]';
+
+-- same 👆
+select *
+from customers 
+where last_name REGEXP 'BR|BU';
+
+
+-- Null is absense of a values
+-- is null
+
+select *
+from customers 
+where phone is null;
+
+select *
+from customers 
+where phone is not null;
+
+-- Exercise: 
+-- gET ORDERS THAT ARE NOT SHIPPED
+
+Select * 
+from orders 
+where shipper_id is null or shipped_date is null;
+
+Select * 
+from orders 
+where shipper_id is null;
+
+Select * 
+from orders 
+where shipped_date is null;
+
+
+-- Order_by Clause
+
+-- Primary key 
+-- In RDBMS every column should have a Primary key 
+-- and the values in that column should uniquely identify the records in that table
+select *
+from customers
+order by first_name;
+
+select *
+from customers
+order by first_name desc;
+
+select *
+from customers
+order by state, first_name;
+
+select *
+from customers
+order by state desc, first_name asc;
+
+select first_name, last_name
+from customers
+order by birth_date;
+
+select first_name, last_name, 10 as points 
+from customers
+order by birth_date;
+
+select first_name, last_name, 10 as points 
+from customers
+order by 1, 2; -- to avoid , always doing it by name
 
 
 
