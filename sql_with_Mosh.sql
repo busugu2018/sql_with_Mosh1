1d4a3e2246ec1a2ce2aa4d950671e3a90d7bd94f
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-06-30 18:23:41 -0500

    commit 6: 1:35:05.

--- sql_with_Mosh.sql
+++ sql_with_Mosh.sql
@@ -474,7 +474,10 @@ on oi.product_id = pr.product_id;
 
 -- combining tables from different DBs
 
-
+select *
+from order_items oi
+join sql_inventory.products p
+on oi.product_id = p.product_id;
 
 
 
