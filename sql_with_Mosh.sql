b1e1986a1147ed5a7616c493e2f5a51005343638
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-09-10 08:34:03 -0500

    commit 15: W3School follow up.

--- sql_with_Mosh.sql
+++ sql_with_Mosh.sql
@@ -808,7 +808,19 @@ join employees m
 
 -- Select all records where the first letter of the City is NOT an "a" or a "c" or an "f".
 
+-- Use the IN operator to select all the records where Country is either "Norway" or "France".
 
+-- Use the IN operator to select all the records where Country is NOT "Norway" and NOT "France".
+
+-- Use the BETWEEN operator to select all the records where the value of the Price column is between 10 and 20.
+
+-- Use the BETWEEN operator to select all the records where the value of the Price column is NOT between 10 and 20.
+
+-- Use the BETWEEN operator to select all the records where the value of the ProductName column is alphabetically between 'Geitost' and 'Pavlova'.
+
+-- When displaying the Customers table, make an ALIAS of the PostalCode column, the column should be called Pno instead.
+
+-- When displaying the Customers table, refer to the table as Consumers instead of Customers.
 
 
 
